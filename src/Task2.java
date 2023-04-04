305310a9ec3e286a42a5ddb152575812cc429368
pouterr <poutergutorov@gmail.com>
2023-04-04 22:20:33 +0600

    Adding comments to code

--- src/Task2.java
+++ src/Task2.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //Defining length of array
         System.out.println("How many elements?");
         int n = sc.nextInt();
         int[] arr = new int[n];
+        //Define all elements of array
         System.out.println("Enter elements of array:");
         for (int i = 0;i<n;i++){
             arr[i] = sc.nextInt();
